e12d209834dddbbc823a1fe5b7994a708c6f5b62
Fadhil Mufid <mufidfadhil@gmail.com>
2025-01-05 18:22:17 +0700

    listen

--- server.js
+++ server.js
@@ -10,6 +10,4 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 // Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
-});
+app.listen(PORT, () => {});
