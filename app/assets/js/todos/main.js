c622be44bc7d986b19300b086780033de35ac22e
Fadhil Mufid <mufidfadhil@gmail.com>
2025-01-05 14:47:28 +0700

    remove state

--- app/assets/js/todos/main.js
+++ app/assets/js/todos/main.js
@@ -1,4 +1,4 @@
-import { elmnState } from "../../../elmn.js";
+// import { elmnState } from "../../../elmn.js";
 
 // Initial todos data
 const initialTodos = [
@@ -26,16 +26,16 @@ async function fetchTodos(limit) {
 
 const addTodo = async (text) => {
   const todosData = await fetchTodos(`${variables.todos.length + 1}`);
-  elmnState("todos", [...todosData]);
-  elmnState("loading", false);
+  // elmnState("todos", [...todosData]);
+  // elmnState("loading", false);
 
-  const filters = ["ongoing", "all", "completed"];
-  let randomIndex = Math.floor(Math.random() * filters.length);
-  elmnState("currentFilter", filters[randomIndex]);
+  // const filters = ["ongoing", "all", "completed"];
+  // let randomIndex = Math.floor(Math.random() * filters.length);
+  // elmnState("currentFilter", filters[randomIndex]);
 
-  // Generate a random hex color
-  let randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
-  elmnState("backgroundColor", randomColor);
+  // // Generate a random hex color
+  // let randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  // elmnState("backgroundColor", randomColor);
 };
 
 // Initialize data first
@@ -65,6 +65,6 @@ export let variables = {
 // Export functions as plain objects
 export let functions = {
   addTodo: addTodo,
-  elmnState: elmnState,
+  // elmnState: elmnState,
   fetchTodos: fetchTodos,
 };
